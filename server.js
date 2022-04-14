b0fd7fb37f452e880139dca115c6ba58538ff5e3
luismrosero <luismrosero1992@gmail.com>
2022-04-13 21:19:45 -0500

    cambio

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-const express = require('express');
+const data = require('ttn').data;
+const application = require('ttn').application
 
-const app = express();
+const appID = "prueba-nueve"
+const accessKey = "NNSXS.4TXTLG2ENYMAQJYQSQWHCKLECRZZUZVLME2T4WA.NKHNWPOTSR625LOJS2EOJX4OSBXKSR5WUVTL2NKNDLVGQMJKPAVA"
 
-app.get('/', (req, res) =>
-    res.send('<h1>Hola nuevamente!!</h1>'))
 
-app.listen(3000);
-console.log("Servidor conectado desde 3000")
+// discover handler and open mqtt connection
+data(appID, accessKey)
+    .then(function (client) {
+        client.on("uplink", function (devID, payload) {
+            console.log("Received uplink from ", devID)
+            console.log(payload)
+        })
+    })
+    .catch(function (err) {
+        console.error(err)
+        process.exit(1)
+    })
+
+// discover handler and open application manager client
+application(appID, accessKey)
+    .then(function (client) {
+        return client.get()
+    })
+    .then(function (app) {
+        console.log("Got app", app)
+    })
+    .catch(function (err) {
+        console.error(err)
+        process.exit(1)
+    })
