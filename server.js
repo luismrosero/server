47d01cef36031e0dd7b715009e0edab287e69533
luismrosero <luismrosero1992@gmail.com>
2022-04-14 21:53:46 -0500

    para prueba

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+'use strict'
 
 const express = require('express');
 const bodyParser = require('body-parser')
@@ -11,17 +11,18 @@ app.use(bodyParser.json())
 
 app.get("/webhook", (req, res) => {
     res.send("Bienvenido a mi webhook");
+    console.log("get");
 })
 
 
 app.listen(port, () => {
     console.log('Servicio corriendo en el puerto ' + port);
 })
-/*
+
 app.post("/webhook", (req, res) => {
     var data = req.body;
     console.log("Inicio del Json recibido");
     console.log(data)
     console.log("Fin del Json recibido");
     res.sendStatus(200)
-})*/
+})
